e8a79ca1704c51463d0d150ea446e70ac13c8927
Makedonskiy-288 <68197222+Makedonskiy-288@users.noreply.github.com>
2021-10-08 21:46:26 +0000

    Deploying to gh-pages from @ Makedonskiy-288/group-9-icecream@fe417efd904315d76fa34f6652485768300fa0af 🚀

--- src.32d8c208.js
+++ src.32d8c208.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/group-9-icecream/src.d137f9dc.js.map
+//# sourceMappingURL=/group-9-icecream/src.32d8c208.js.map
