4e5d400d713a412cbe33e3268dc76746d81d1ca5
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2021-10-12 20:53:36 +0300

    Modal-about-fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,6 +16,24 @@
     bodyClose.classList.toggle('overflowed');
   }
 })();
+// Modal-about
+(() => {
+   const bodyClose = document.querySelector('body');
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-about-open]'),
+    closeModalBtn: document.querySelector('[data-modal-about-close]'),
+    modal: document.querySelector('[data-modal-about]'),
+    
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    bodyClose.classList.toggle('overflowed');
+  }
+})();
 
 // Contacts-map
 (() => {
